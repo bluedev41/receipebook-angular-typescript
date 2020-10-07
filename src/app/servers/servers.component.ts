2a43eefcc63ce9d98f44801ff137c1d6715d66c9
YuDev0401 <lollanoyer@gmail.com>
2020-10-07 19:54:34 +0530

    Modifications to Servers Component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+	selector: 'app-servers',
+	templateUrl: './servers.component.html',
+	styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+	allowNewServer: boolean = false;
+	serverCreationStatus: string = 'No server was created';
+	serverName: string = '';
 
-  constructor() { }
+	constructor() {
+		setTimeout(() => {
+			this.allowNewServer = true;
+		}, 2000);
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	onCreateServer = () => {
+		this.serverCreationStatus =
+			'Server was created! Name is ' + this.serverName;
+	};
+
+	// Function to capture the input value and store it in the variable
+	// onUpdateServerName = (e: any) => {
+	// 	this.serverName = e.target.value;
+	// 	console.log(this.serverName);
+	// };
 }
